d5ca416a26adf7a6a7fb30e1968c2627acfd9615
risdariaa <risdariaria1@gmail.com>
2019-06-26 20:35:38 +0800

    versi awal

---
+++ tugas1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main () 
+{	
+	char nama [32];
+	char kelas [12];
+	char nim [12];
+	char alamat [50];
+	char fakultas [50];
+	
+	cout<<"=====TUGAS 6 SIMBADA===="
+	<<endl;
+	cout<<"===BIODATA MAHASISWA==="
+	<<endl;
+	cout<<"nama: ";
+	cin>>nama;
+	cout<<"kelas: ";
+	cin>>kelas;
+	cout<<"nim: ";
+	cin>>nim;
+	cout<<"alamat: ";
+	cin>>alamat;
+	cout<<"fakultas: ";
+	cin>>fakultas;
+	getch ();
+	
+}
